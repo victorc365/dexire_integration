8f2247f784dfde06c3b40dfe260e1bdc0d2dc7d8
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-06-23 16:19:04 +0200

    feat: first skeleton of backend api

---
+++ core/src/enums/environment.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Environment(Enum):
+    LOG_DIRECTORY_PATH = 'LOG_DIRECTORY_PATH'
+    LOG_LEVEL = 'LOG_LEVEL'
---
+++ core/src/main.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from api.v1 import router
+from enums.environment import Environment
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def init_logger():
+    LOG_DIRECTORY_PATH = os.environ.get(Environment.LOG_DIRECTORY_PATH.value)
+    LOG_LEVEL = os.environ.get(Environment.LOG_LEVEL.value)
+    logging.basicConfig(level=LOG_LEVEL,
+                        format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+                        filename=f'{LOG_DIRECTORY_PATH}/logs.txt')
+    return logging.getLogger(__name__)
+
+def init_fast_api():
+    app = FastAPI(docs_url='/docs', redoc_url=None)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.include_router(router.api_router)
+    return app
+
+logger = init_logger()
+app = init_fast_api()
+logger.debug('application initialized')
+
