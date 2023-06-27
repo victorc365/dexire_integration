b1d1fb3e57c354e5ba2e8b5eb43cd86254d364e0
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-06-27 15:12:50 +0200

    refactor: improve main.py readability

--- core/src/main.py
+++ core/src/main.py
@@ -1,6 +1,11 @@
-from setup import init_fast_api, init_logger
+import nest_asyncio
+import setup
 
-logger = init_logger()
-app = init_fast_api()
-logger.debug('application initialized')
+# https://medium.com/@vyshali.enukonda/how-to-get-around-runtimeerror-this-event-loop-is-already-running-3f26f67e762e
+# Spade and unicorn are both using asyncio and this create a conflict because asyncio does not allow nested event loops.
+# nest_asyncio is a patch for this.
+# TODO - Remove this library when the official asyncio library is patched.
+nest_asyncio.apply()
 
+setup.init_logger()
+setup.init_fast_api()
