df8b8b6734b70dbe5cc17045a45488b541fc8d5b
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-07-17 18:10:23 +0200

    fix: correct problems with environment and docker building

--- core/src/utils/string_builder.py
+++ core/src/utils/string_builder.py
@@ -1,3 +1,5 @@
+import os
+
 from enums.environment import Environment
 
 
@@ -6,4 +8,4 @@ class StringBuilder():
 
 
 def create_jid(name: str) -> str:
-    return f'{name}@{Environment.XMPP_SERVER_URL.value}'
+    return f'{name}@{os.environ.get(Environment.XMPP_SERVER_URL.value)}'
