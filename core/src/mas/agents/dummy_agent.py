0dae662813354b75a345fd333a60d5b12bb6b2a7
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-07-24 16:13:26 +0200

    feat: dummy agent kill itself after  10sec

--- core/src/mas/agents/dummy_agent.py
+++ core/src/mas/agents/dummy_agent.py
@@ -10,16 +10,20 @@ class DummyAgent(BasicAgent):
 
     async def setup(self):
         self.logger.info('starting...')
-        behaviour = self.DummyBehaviour(self.logger)
+        behaviour = self.DummyBehaviour(self, self.logger)
         self.add_behaviour(behaviour)
 
     class DummyBehaviour(CyclicBehaviour):
-        def __init__(self, logger):
+        def __init__(self, agent, logger):
             super().__init__()
             self.counter = 0
             self.logger = logger
+            self.agent = agent
 
         async def run(self):
             self.logger.debug('count : ' + str(self.counter))
             self.counter += 1
+            if self.counter == 10:
+                self.kill(exit_code=10)
+                await self.agent.stop()
             await asyncio.sleep(1)
