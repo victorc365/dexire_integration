bc48b6357d28af429d0b82265574351e50ce1f8c
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-08-31 17:59:44 +0200

    feat: add chat_service to handle websocket forwarding

---
+++ core/src/services/chat_service.py
@@ -0,0 +1,22 @@
+from utils.metaclasses.singleton import Singleton
+from mas.core_engine import CoreEngine
+
+
+class ChatService(metaclass=Singleton):
+    def __init__(self):
+        self.bot_gateways = {}
+
+    def register_gateway(self, gateway_id: str, bot_user_name: str):
+        bot_user_name = bot_user_name.split('@')[0]
+        client_id = bot_user_name.split('_')[1]
+        if client_id not in self.bot_gateways.keys():
+            self.bot_gateways[client_id] = {}
+
+        print(client_id)
+        print(bot_user_name)
+        self.bot_gateways[client_id][bot_user_name] = gateway_id
+
+    async def  forward_websocket(self, websocket, client_id: str, bot_user_name: str):
+        gateway_id = self.bot_gateways[client_id][bot_user_name]
+        gateway = CoreEngine().container.get_agent(gateway_id)
+        await gateway.register_websocket(bot_user_name,websocket)
