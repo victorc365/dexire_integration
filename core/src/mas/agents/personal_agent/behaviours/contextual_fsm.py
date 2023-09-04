48ef042bec9f8bbc4a6e9017230bfe507b2f787a
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-09-04 13:46:56 +0200

    docs: Add documentation for personal agent behaviour

--- core/src/mas/agents/personal_agent/behaviours/contextual_fsm.py
+++ core/src/mas/agents/personal_agent/behaviours/contextual_fsm.py
@@ -2,4 +2,12 @@ from spade.behaviour import FSMBehaviour
 
 
 class ContextualFSMBehaviour(FSMBehaviour):
-    pass
+    """ Main state machine behaviour of a module (bot).
+
+    A Personal Agent must have a ContextualFSMBehaviour. It is the state machine that is in charge to execute the module
+    main purpose. It defines the main behaviour of the bot. This state machine is loaded dynamically from the
+    installation directory of the module and will be executed by all Personal Agents created for this specific module.
+    """
+
+    def __init__(self) -> None:
+        pass
--- core/src/mas/agents/personal_agent/behaviours/profiling_fsm.py
+++ core/src/mas/agents/personal_agent/behaviours/profiling_fsm.py
@@ -2,4 +2,10 @@ from mas.agents.generic_behaviours.dynamic_fsm import DynamicFSMBehaviour
 
 
 class ProfilingFSMBehaviour(DynamicFSMBehaviour):
-    pass
+    """ State machine used for user profiling.
+
+    This state machine behaviour provides to the personal agent some capabilities to profile a user.
+    As different chat bot may need different information from the user, the list of questions for the profiling is
+    fully customizable and the profiling behaviour is built using a list of questions provided as a yaml file coming
+    with the bot module.
+    """
