304c2d24abfaf0198623dee03dcfed20dd193e22
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-09-29 15:25:40 +0200

    feat: add contacts and currentBot to provider

--- hemerapp/lib/providers/bots_provider.dart
+++ hemerapp/lib/providers/bots_provider.dart
@@ -5,15 +5,22 @@ import 'package:hemerapp/repositories/bots_repository.dart';
 class BotsProvider with ChangeNotifier {
   final _repository = BotsRepository();
   bool isLoading = false;
+  BotModel? currentBot;
   List<BotModel> _bots = [];
+  List<BotModel> _contacts = [];
 
   List<BotModel> get bots => _bots;
 
-  Future<void> getAllBots() async {
+  List<BotModel> get contacts => _contacts;
+
+  Future<void> getAllBots(String? username) async {
     isLoading = true;
     notifyListeners();
-    final response = await _repository.fetchBots();
-    _bots = response;
+    _bots = await _repository.fetchBots();
+    if (username != null) {
+      _contacts = await _repository.fetchContacts(username);
+    }
+
     isLoading = false;
     notifyListeners();
   }
