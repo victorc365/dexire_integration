083460fcb9e33e7d080d21aa1f1450e0e5a80226
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-10-04 16:41:34 +0200

    misc: remove unused contextual fsm

--- modules/devbot3/contextual_fsm.py
+++
@@ -1,9 +0,0 @@
-from mas.agents.personal_agent.behaviours.contextual_fsm import AbstractContextualFSMBehaviour
-
-
-class ContextualFSM(AbstractContextualFSMBehaviour):
-    def __init__(self):
-        super().__init__()
-
-    async def on_start(self):
-        await super().on_start()
