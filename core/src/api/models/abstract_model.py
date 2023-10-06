f96a589d88b7f896a3a31b072c1aaed5220303c1
Yvan Pannatier <yvan.pannatier@hevs.ch>
2023-10-06 10:23:00 +0200

    refactor: fix typo

--- core/src/api/models/abstract_model.py
+++ core/src/api/models/abstract_model.py
@@ -10,10 +10,10 @@ class AbstractModel(BaseModel):
     have to extend this instead of BaseModel.
     """
     model_config = ConfigDict(
-        # remove whitespaces in the beggining / end of strings
+        # remove whitespaces in the beginning / end of strings
         str_strip_whitespace=True,
         # allows to use either variable name or alias to populate a field of the model
         populate_by_name=True,
-        # allows to instanciate model from attribute of another class
+        # allows to instantiate model from attribute of another class
         from_attributes=True,
     )
